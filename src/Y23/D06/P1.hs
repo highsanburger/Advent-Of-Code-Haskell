b6c8e1ec091557bd3c8914f72822a2c71c43c503
Syed Khalid <magmastar167@gmail.com>
2023-12-07 22:10:45 +0300

    lol aoc so ez d6

--- src/Y23/D06/P1.hs
+++ src/Y23/D06/P1.hs
@@ -1,17 +1,27 @@
 module Y23.D06.P1 where
 
-import qualified Data.Char as C
-import qualified Data.Either as E
-import qualified Data.List as L
-import qualified Data.Maybe as M
-import qualified Data.Text as T
-import qualified Data.Vector as V
+parse :: String -> [(Int, Int)]
+parse input = zip time dist
+  where
+    time = map read $ tail $ words $ takeWhile (/= '\n') input
+    dist = map read $ tail $ words $ dropWhile (/= '\n') input
+
+allTimes :: (Int, Int) -> [(Int, Int)]
+allTimes (t, _) = [(x, x * (t - x)) | x <- [0 .. t]]
+
+isWinning :: (Int, Int) -> (Int, Int) -> Bool
+isWinning (t, d) (t', d') = d' > d
+
+ways :: (Int, Int) -> Int
+ways (t, d) = length $ filter (isWinning (t, d)) $ allTimes (t, d)
 
 solve :: String -> Int
-solve inp = undefined
+solve inp = product $ map ways $ parse inp
 
 main :: IO ()
 main = do
-  -- input <- readFile "src/Y23/D06/input.txt"
-  input <- readFile "src/Y23/D06/example.txt"
+  input <- readFile "src/Y23/D06/input.txt"
+  -- input <- readFile "src/Y23/D06/example.txt"
   print $ solve input
+
+-- 30 seconds meh
--- src/Y23/D06/P2.hs
+++ src/Y23/D06/P2.hs
@@ -2,11 +2,17 @@ module Y23.D06.P2 where
 
 import Y23.D06.P1
 
+parse2 :: String -> (Int, Int)
+parse2 input = (time, dist)
+  where
+    time = read $ concat $ tail $ words $ takeWhile (/= '\n') input
+    dist = read $ concat $ tail $ words $ dropWhile (/= '\n') input
+
 solve2 :: String -> Int
-solve2 inp = undefined
+solve2 inp = ways $ parse2 inp
 
 main2 :: IO ()
 main2 = do
-  -- input <- readFile "src/Y23/D06/input.txt"
-  input <- readFile "src/Y23/D06/example.txt"
+  input <- readFile "src/Y23/D06/input.txt"
+  -- input <- readFile "src/Y23/D06/example.txt"
   print $ solve2 input
